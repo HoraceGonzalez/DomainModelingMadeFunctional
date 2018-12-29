b502ed9dbc6ed9d54f7e38410ad64d49089a92a4
Horace Gonzalez <horace.r.gonzalez@gmail.com>
2018-12-29 11:28:16 -0800

    updated with code from https://pragprog.com/titles/swdddf/source_code

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#---
+# Excerpted from "Domain Modeling Made Functional",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/swdddf for more book information.
+#---
 if test "$OS" = "Windows_NT"
 then
   # use .Net
